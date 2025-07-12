9756293e836b5c0cb7f0107408af3486924fc88a
AMALARAR <amalarar@amdocs.com>
2025-07-13 01:09:56 +0530

    special char remove

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -50,7 +50,7 @@ const Testimonials = () => {
             What Our Clients Say
           </h2>
           <p className="text-xl text-gray-600 max-w-3xl mx-auto">
-            Don't just take our word for it - hear from our satisfied clients
+            Don&apos;t just take our word for it - hear from our satisfied clients
           </p>
         </motion.div>
 
